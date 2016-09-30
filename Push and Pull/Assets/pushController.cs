6ea0f539fdf1ffb115440743ba772c0cc6d93f60
Sam Reha <sreha@ucsc.edu>
2016-09-29 18:46:56 -0700

    Initial commit, I guess

---
+++ Push and Pull/Assets/pushController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pushController : MonoBehaviour {
+    public GameObject settings;
+
+    // Use this for initialization
+    void Start () {
+    
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetButtonDown("MovePushUp")) {
+            transform.Translate(Vector3.up * Time.deltaTime);
+        }
+        if (Input.GetButtonDown("MovePushDown")) {
+
+        }
+        if (Input.GetButtonDown("MovePushLeft")) {
+
+        }
+        if (Input.GetButtonDown("MovePushRight")) {
+
+        }
+    }
+}
